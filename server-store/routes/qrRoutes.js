7d49359a1d2735448bcb595c3ad80e9dfbdce4a7
Nilesh Gurav <nileshgurav1718@gmail.com>
2024-12-15 01:56:40 +0530

    Connected Upload url in generateqr page

--- server-store/routes/qrRoutes.js
+++ server-store/routes/qrRoutes.js
@@ -16,7 +16,7 @@ router.get('/generate', async (req, res) => {
     const user = await User.findOne({ email });
     if (!user) return res.status(404).send('User not found');
 
-    const uploadPageUrl = `http://localhost:3002/upload?storeid=${encodeURIComponent(user.uniqueId)}`;
+    const uploadPageUrl = `https://scan2-print-app-upload.vercel.app/upload?storeid=${encodeURIComponent(user.uniqueId)}`;
     const qrCodeDataUrl = await QRCode.toDataURL(uploadPageUrl);
 
     res.status(200).json({ qrCodeDataUrl, uploadPageUrl });
